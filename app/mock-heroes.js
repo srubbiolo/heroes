bfb2d592f882d47ecabacff6348895d038fbc62b
srubbiolo <rubbiolo@gmail.com>
2016-12-22 17:38:32 -0300

    Add files via upload

---
+++ app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 22, name: 'El cabezón' },
+    { id: 18, name: 'Camilo' },
+    { id: 13, name: 'El pipa' },
+    { id: 7, name: 'El Rorro' },
+    { id: 11, name: 'El gordo Driussi' },
+    { id: 23, name: 'El león Ponzio' },
+    { id: 2, name: 'El Jhonny' },
+    { id: 4, name: 'El Paragua Moreira' },
+    { id: 10, name: 'El pitty' },
+    { id: 5, name: 'Arzura' }
+];
+//# sourceMappingURL=mock-heroes.js.map
